c05a31f73cde96afdf28f66d379917cc4ffa44a8
Vlad Bibire <vlad@3sidedcube.com>
2023-05-30 14:27:27 +0300

    Component created

---
+++ Sources/Components/ARCImageContainer.swift
@@ -0,0 +1,21 @@
+//
+//  ARCImageContainer.swift
+//  ArcUI
+//
+//  Created by Vlad Bibire on 30/05/2023.
+//  Copyright © 2023 3 SIDED CUBE APP PRODUCTIONS LTD. All rights reserved.
+//
+
+import SwiftUI
+
+struct ARCImageContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ARCImageContainer_Previews: PreviewProvider {
+    static var previews: some View {
+        ARCImageContainer()
+    }
+}
