1bfd2bac4b02234d2260c876a3e26ac5a2843137
Marliza <marliza@3sidedcube.com>
2023-07-31 10:52:13 +0100

    update versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "ArcUI",
     platforms: [
         // .macOS(.v12), Uses UIKit
-        .iOS(.v15)
+        .iOS(.v16)
     ],
     products: [
         .library(
