9cd7a7d0bc07bb5d6aaf983d2b0b5d8a533bde1a
Neil Japhtha <neil@3sidedcube.com>
2025-09-24 16:45:15 +0100

    Add reusable arc page header component (#31)
    * Add reusable arc page header component
    
    * Remove unused veerticalSizeClass

---
+++ Sources/Components/ARCPageHeaderView.swift
@@ -0,0 +1,27 @@
+//
+//  ARCPageHeaderView.swift
+//  ArcUI
+//
+//  Created by Neil Japhtha on 23/09/2025.
+//  Copyright © 2025 3 SIDED CUBE APP PRODUCTIONS LTD. All rights reserved.
+//
+
+import SwiftUI
+
+struct ARCPageHeaderView<Content: View>: View {
+    let backgroundColor: Color
+    let content: () -> Content
+    
+    let horizontalPadding: CGFloat
+    let verticalPadding: CGFloat
+
+    var body: some View {
+        content()
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal, horizontalPadding)
+            .padding(.vertical, verticalPadding)
+            .background(
+                backgroundColor
+            )
+    }
+}
