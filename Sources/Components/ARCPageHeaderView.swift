34cefb3a0afa5ddc3bead52875eb07b491706812
Neil Japhtha <neil@3sidedcube.com>
2025-09-26 11:18:46 +0100

    Make sure the ARCPageHeaderView can be constructed outside of the package (#33)
    * Add reusable arc page header component
    
    * Remove unused veerticalSizeClass
    
    * Make sure struct is public

--- Sources/Components/ARCPageHeaderView.swift
+++ Sources/Components/ARCPageHeaderView.swift
@@ -8,14 +8,26 @@
 
 import SwiftUI
 
-struct ARCPageHeaderView<Content: View>: View {
+public struct ARCPageHeaderView<Content: View>: View {
     let backgroundColor: Color
     let content: () -> Content
     
     let horizontalPadding: CGFloat
     let verticalPadding: CGFloat
 
-    var body: some View {
+    public init(
+        backgroundColor: Color,
+        content: @escaping () -> Content,
+        horizontalPadding: CGFloat,
+        verticalPadding: CGFloat
+    ) {
+        self.backgroundColor = backgroundColor
+        self.content = content
+        self.horizontalPadding = horizontalPadding
+        self.verticalPadding = verticalPadding
+    }
+
+    public var body: some View {
         content()
             .frame(maxWidth: .infinity)
             .padding(.horizontal, horizontalPadding)
